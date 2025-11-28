cea1ec31b253ea82b2b5542b7b6507a915d38ab4
GO <93365142+beadskode@users.noreply.github.com>
2025-11-28 16:31:03 +0900

    Title: 특정 수 도달하기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251128/특정 수 도달하기/reaching-specific-number.js
+++ 251128/특정 수 도달하기/reaching-specific-number.js
@@ -7,7 +7,7 @@ function getValue() {
     for (let i = 0; i < input.length; i++) {
         if (input[i] >= 250) return `${sum} ${aver}`;
         sum += input[i];
-        aver = Math.round(sum / (i+1)).toFixed(1);
+        aver = (Math.round(sum / (i+1) * 10) / 10).toFixed(1);
     }
     return `${sum} ${aver}`;
 }
