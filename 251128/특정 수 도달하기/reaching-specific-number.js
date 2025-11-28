169fe0f064a92190d1719113aa52bf441ad239a2
GO <93365142+beadskode@users.noreply.github.com>
2025-11-28 16:21:58 +0900

    Title: 특정 수 도달하기, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251128/특정 수 도달하기/reaching-specific-number.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+function getValue() {
+    let sum = 0;
+    let aver = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] >= 250) return `${sum} ${aver}`;
+        sum += input[i];
+        aver = sum / (i+1);
+    }
+}
+
+console.log(getValue())
